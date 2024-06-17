06cb94f5e7d86a33da571055ed1eefe9c6b55582
andreamsilu <msiluanrew2020@gmail.com>
2024-06-17 11:13:30 +0300

    user footer added

---
+++ includes/footer.php
@@ -0,0 +1,54 @@
+<footer class="bg-light text-center text-lg-start mt-4">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">About Us</h5>
+                <p>
+                    We are a leading tourism portal dedicated to helping you explore the world with ease and comfort. Join us for unforgettable journeys.
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Links</h5>
+                <ul class="list-unstyled mb-0">
+                    <li>
+                        <a href="index.php" class="text-dark">Home</a>
+                    </li>
+                    <li>
+                        <a href="destinations.php" class="text-dark">Destinations</a>
+                    </li>
+                    <li>
+                        <a href="tours.php" class="text-dark">Tours</a>
+                    </li>
+                    <li>
+                        <a href="bookings.php" class="text-dark">Bookings</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <ul class="list-unstyled mb-0">
+                    <li>
+                        <p><i class="fas fa-map-marker-alt"></i> 1234 Street, City, Country</p>
+                    </li>
+                    <li>
+                        <p><i class="fas fa-envelope"></i> info@tourismportal.com</p>
+                    </li>
+                    <li>
+                        <p><i class="fas fa-phone"></i> +123 456 7890</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-3 bg-light">
+        © 2024 Tourism Portal. All rights reserved.
+    </div>
+</footer>
+
+<!-- Bootstrap JS and dependencies -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
